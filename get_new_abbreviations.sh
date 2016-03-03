18e36a7c670a25152b7bcc37e3bb590e4874be19
Jackson Souza <jacksonjsouza@gmail.com>
2016-03-02 22:34:04 -0300

    adição de script para verificar se todos os acordãos possuem inteiros teores, relatório da última execução do scraper para coleta de decisões, alteração do script de coleta de mensagens de log, adição de mensagem em spider de decisões monocráticas para saber se estásendo extraído o número correto de decisões de cada página buscada e adição de novas informações para fazer o dump e recuperação do banco de dados

--- get_new_abbreviations.sh
+++ get_new_abbreviations.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cat decisao.log | sed -nr  's/.*(\[NEW ABV\].*)/\1/p' | sort | uniq
+cat decisao_* | sed -nr  's/.*(\[NEW ABV\].*)/\1/p' | sort | uniq > abreviacoes_nao_previstas.txt
+cat decisao_* | sed -nr  's/.*(\[LOOK ABBRV\].*)/\1/p' | sort | uniq > inpecionar_abreviacoes.txt
 
+# outras buscas
+# "possui mais de 10 documentos na"
+# "nao possui inteiro teor!"
+# "possui menos de 10 documentos na página"
+# "We have a new section called"
 # http://www.grymoire.com/Unix/Sed.html#uh-15
