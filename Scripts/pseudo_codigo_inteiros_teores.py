9ebb2397d16f4c6871bac031dc9da824bafedfba
Jackson Souza <jacksonjsouza@gmail.com>
2016-09-20 23:55:38 -0300

    reorganização de scripts que realizam operações diversas sobre as decisões

---
+++ Scripts/pseudo_codigo_inteiros_teores.py
@@ -0,0 +1,29 @@
+
+import glob
+from pymongo import MongoClient
+import os
+import re
+
+# O script foi criado com o propósito de remover inteiros teores baixados que possuem
+# nomes que possuem uma padrão estranho 
+path = "/home/jackson/analise-juridica/scrapy/inteiros_teores/full/*"
+files = glob.glob(path)
+
+files_dict = {}
+for f in files:
+    f_match = re.search(r"[^/]/([^/]+)$", f)
+    files_dict[f_match.groups(1)[0]] = f
+
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['DJs']
+coll = db['acordaos']
+
+for doc in coll.find({}, {"files": 1, "_id":0}):
+	f_match = re.search(r"[^/]/([^/]+)$", doc["files"])
+	files_dict[f_match.groups(1)[0]] = None
+
+files_to_delete = filter(lambda value: value is not None, files_dict.values())
+
+for complete_path in files_to_delete:
+    os.remove(complete_path)
