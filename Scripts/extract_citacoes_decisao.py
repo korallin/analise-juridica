7a00ac93f688aaf739dc84c224dca2cea0400916
Jackson Souza <jacksonjsouza@gmail.com>
2017-02-12 15:15:36 -0200

    removendo bug que impedia atualização dos documentos

--- Scripts/extract_citacoes_decisao.py
+++ Scripts/extract_citacoes_decisao.py
@@ -13,30 +13,26 @@ decisoes_monocraticas = db.decisoes_monocraticas
 
 for coll in [acordaos, decisoes_monocraticas]:
     for doc in coll.find({}):
-        try:
-            citacoes = re.findall(r"([A-Z][a-zA-Z]+)\s+([nN].\s+)?([0-9]+((\.[0-9]{3})+)?)((\/\s*[A-Z]+)((\s+|\-|–)[A-Z][a-zA-Z]+(((\-|–)[A-Z][a-zA-Z]+)+)?)?|((\-|–|\s+)[A-Z][a-zA-Z]+(((\-|–)[A-Z][a-zA-Z]+)+)?))", doc['decisao'])
+        citacoes = re.findall(r"([A-Z][a-zA-Z]+)\s+([nN].\s+)?([0-9]+((\.[0-9]{3})+)?)((\/\s*[A-Z]+)((\s+|\-|–)[A-Z][a-zA-Z]+(((\-|–)[A-Z][a-zA-Z]+)+)?)?|((\-|–|\s+)[A-Z][a-zA-Z]+(((\-|–)[A-Z][a-zA-Z]+)+)?))", doc['decisao'])
 
-            citacoesDec = set()
-            for citacao in citacoes:
-                acao_originaria = citacao[0]
-                decisao_numero = citacao[2].replace('.', '')
-                classes_processuais_str = re.sub('^(\/[A-Z]+\s*(\-|–)|(\-|–|\s+))', '', citacao[7])
-                if classes_processuais_str == "":
-                    classes_processuais_str = re.sub('^(\/[A-Z]+\s*(\-|–)|(\-|–|\s+))', '', citacao[12])
+        citacoesDec = set()
+        for citacao in citacoes:
+            acao_originaria = citacao[0]
+            decisao_numero = citacao[2].replace('.', '')
+            classes_processuais_str = re.sub('^(\/[A-Z]+\s*(\-|–)|(\-|–|\s+))', '', citacao[7])
+            if classes_processuais_str == "":
+                classes_processuais_str = re.sub('^(\/[A-Z]+\s*(\-|–)|(\-|–|\s+))', '', citacao[12])
 
-                classes_processuais_list = classes_processuais_str.split('-')[::-1]
+            classes_processuais_list = classes_processuais_str.split('-')[::-1]
 
-                decisao_codigo = " ".join(classes_processuais_list) + " " + acao_originaria + " " + decisao_numero
-                citacoesDec.add(decisao_codigo.strip())
+            decisao_codigo = " ".join(classes_processuais_list) + " " + acao_originaria + " " + decisao_numero
+            citacoesDec.add(decisao_codigo.strip())
 
-            db.coll.update_one({ "_id" : doc['_id']},
-                            {
-                                "$set": {
-                                    "citacoesDec": list(citacoesDec)
-                                }
-                            })
-        except Exception as e:
-            print e
-            from IPython import embed; embed()
+        coll.update_one({ "_id" : doc['_id']},
+                        {
+                            "$set": {
+                                "citacoesDec": list(citacoesDec)
+                            }
+                        })
 
 
