f0743d8c5a535e7c91ce99eae4896c0c1b5d8b68
Jackson Souza <jacksonjsouza@gmail.com>
2017-07-13 23:24:56 -0300

    reorganizando estrutura de tarefas e diretórios part 5

--- scrapy/runstf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scrapy crawl -a "iDate=20120102" -a "fDate=20140101" -a "page"=1 -a "index"=1 stf_decisao_monocratica --logfile='decisao_monocratica.log'
--- scrapy/runstj.sh
+++
@@ -1,1 +0,0 @@
-scrapy crawl -a "iDate=20030101" -a "fDate=20040101" -a "update"=0 stj
