acd3b5499e125cdd2db1be35673f87d443c115d5
Jackson Souza <jacksonjsouza@gmail.com>
2020-04-11 20:19:46 -0300

    adding kleinberg analysis

---
+++ analysis_programs/NetworkXDigraph.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from Acordao import Acordao
+from pymongo import MongoClient
+import networkx as nx
+
+
+class NetworkXDigraph:
+    def __init__(
+        self,
+        mongo_uri,
+        db_name,
+        collection_in,
+        collection_out_name,
+    ):
+        client = MongoClient(mongo_uri)
+        self.db = client[db_name]
+        self.collection_in = collection_in
+        self.collection_out = self.db[collection_out_name]
+        self.collection_out.drop()
+        self.one_percent = sum([coll.count() for coll in self.collection_in]) / 100
+
+    def save_removed_decisions(self, i, removed_decisions, collection_out_name):
+        removed_coll = self.db[collection_out_name + "_removed_%d" % i]
+        removed_coll.drop()
+
+        removed_coll.insert_one(
+            {"iteration": i, "removed_decisions": removed_decisions}
+        )
+
+    def set_collections_out(self, collection_out_name):
+        self.collection_out = self.db[collection_out_name]
+        self.collection_out.drop()
+
+    def build_graph(self, query, removed_decisions, compute_similars):
+        acordaos = {}
+        G = nx.DiGraph()
+
+        print("building map")
+
+        self.count = self.progress = 0
+
+        for coll in self.collection_in:
+            docsFound = coll.find(query, no_cursor_timeout=True)
+            for doc in docsFound:
+                if doc["acordaoId"] in removed_decisions:
+                    continue
+
+                # ADICIONA NÓ
+                docId = doc["acordaoId"]
+                G.add_node(docId) if docId not in acordaos else None
+                acordaos[docId] = Acordao(docId, doc["tribunal"], doc["relator"], False)
+                # ADICIONA ARCOS
+                for ac_cit in doc["citacoesObs"]:
+                    if ac_cit not in acordaos:
+                        G.add_node(ac_cit)
+                        acordaos[ac_cit] = Acordao(ac_cit, "", "", False)
+
+                    G.add_edge(docId, ac_cit)
+
+                if compute_similars == "S":
+                    for similar in doc["similares"]:
+                        similarId = similar["acordaoId"]
+                        if similarId not in removed_decisions:
+                            for quotedId in doc["citacoesObs"]:
+                                if similarId not in acordaos:
+                                    G.add_node(similarId)
+                                    acordaos[similarId] = Acordao(
+                                        similarId, doc["tribunal"], similar["relator"], True
+                                    )
+
+                                G.add_edge(similarId, quotedId)
+
+                self.__print_progress()
+
+            print("")
+
+        return [G, acordaos]
+
+    # Insert nodes and authority and hub node values
+    def insert_nodes(self, G, acordaos, authorities, hubs):
+        n_docs = len(acordaos)
+        self.one_percent = n_docs / 100
+        self.count = self.progress = 0
+        insert_step = n_docs
+        if n_docs > 10000:
+            insert_step = 10000
+
+        print("n acordaos %s to be inserted" % n_docs)
+
+        i = 0
+        docs_to_insert = []
+        for docId, doc in acordaos.items():
+            docs_to_insert.append(
+                {
+                    "acordaoId": docId,
+                    # "citacoes": doc.getCitacoes(),
+                    "citadoPor": [tup[0] for tup in G.out_edges(docId)],
+                    # "similares": doc.getSimilares(),
+                    "relator": doc.getRelator(),
+                    "tribunal": doc.getTribunal(),
+                    "authority": authorities[docId],
+                    "hub": hubs[docId],
+                    "virtual": doc.getVirtual(),
+                }
+            )
+            i += 1
+            self.__print_progress()
+            if i >= insert_step:
+                self.collection_out.insert(docs_to_insert)
+                docs_to_insert = []
+                i = 0
+
+        print("")
+        if i > 0:
+            self.collection_out.insert(docs_to_insert)
+
+
+    def __print_progress(self):
+        self.count += 1
+        if self.count >= self.one_percent:
+            self.count = 0
+            self.progress += 1
+            sys.stdout.write("\r%d%%" % self.progress)
+            sys.stdout.flush()
---
+++ analysis_programs/kleinberg_analysis.py
@@ -0,0 +1,164 @@
+import os
+from math import ceil
+from random import randint
+from multiprocessing import Pool
+from pymongo import MongoClient
+from NetworkXDigraph import NetworkXDigraph
+
+
+def get_decisions_ids(collections, query):
+    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+
+    client = MongoClient(MONGO_URI)
+    db = client[MONGO_DATABASE]
+
+    decisions_ids = []
+    colls = []
+    if collections == "acordaos":
+        colls.append(db["acordaos"])
+    elif collections == "decisoes_monocraticas":
+        colls.append(db["decisoes_monocraticas"])
+    elif collections == "decisoes":
+        colls.append(db["acordaos"])
+        colls.append(db["decisoes_monocraticas"])
+
+    for coll in colls:
+        docs = coll.find(query, no_cursor_timeout=True)
+        for doc in docs:
+            decisions_ids.append(doc["acordaoId"])
+
+    return decisions_ids, colls
+
+
+def get_removed_decisions(decisions_ids, percentage):
+    removed_decisons_len = ceil(len(decisions_ids) * (percentage / 100.0))
+    decisions_ids_len = len(decisions_ids)
+    removed_decisions = []
+    i = 0
+    while i < removed_decisons_len:
+        x = randint(0, decisions_ids_len - 1)
+        if decisions_ids[x] not in removed_decisions:
+            removed_decisions.append(decisions_ids[x])
+            i += 1
+
+    return removed_decisions
+
+
+def get_top_10_relatores():
+    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+    client = MongoClient(MONGO_URI)
+    db = client[MONGO_DATABASE]
+    docs = db["acordaos"].aggregate(
+        [
+            {"$group": {"_id": "$relator", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10},
+        ]
+    )
+    relatores = [doc["_id"] for doc in docs]
+
+    return relatores
+
+
+def run_hits_execution(args):
+    i, query, collections_name, percentage, compute_similars, collection_out_iter_name = args
+    print("execution: ", i, collection_out_iter_name)
+
+    decisions_ids, collections = get_decisions_ids(
+        collections_name, query
+    )
+
+    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+    graph = NetworkXDigraph(
+        MONGO_URI,
+        MONGO_DATABASE,
+        collections,
+        collection_out_iter_name,
+    )
+    if i == 1:
+        removed_decisions = []
+    else:
+        removed_decisions = get_removed_decisions(decisions_ids, percentage)
+
+    graph.save_removed_decisions(i, removed_decisions, collection_out_iter_name)
+
+    # Construct graph
+    [G, acordaos] = graph.build_graph(query, removed_decisions, compute_similars)
+
+    # Se esta função fosse executada seriam removidos acórdãos fora do intervalo temporal
+    # [quotes, quotedBy] = graph.removeInvalidAcordaosFromDicts(
+    #     acordaos, quotes, quotedBy
+    # )
+
+    print("Início da execução do kleinberg:", len(acordaos))
+    # KLEINBERG authorities and hubs
+    hubs, authorities = nx.hits(G, max_iter=1000)
+    graph.set_collections_out(collection_out_iter_name + "_{}".format(i))
+
+    # Insert results
+    graph.insert_nodes(G, acordaos, authorities, hubs)
+
+
+def run_acordaos_kleinberg_experiments():
+    # parâmetros
+    # relatores: "S" ou "N"
+    query = {}
+    collections_name = "acordaos"
+    compute_similars = "S"
+    kleinberg_iters = []
+
+    # experimento kleinberg todas as decisões
+    percentages = [10, 20, 30]
+    for percentage in percentages:
+        collection_out_iter_name = "stf_kleinberg_acordaos_{}".format(percentage)
+        kleinberg_iters.extend([
+            (i, query, collections_name, percentage, compute_similars, collection_out_iter_name)
+            for i in range(1, 11)
+        ])
+
+    compute_similars = "N"
+    percentage = 10
+    collection_out_iter_name = "stf_kleinberg_acordaos_{}_no_similars".format(percentage)
+    kleinberg_iters.extend([
+        (i, query, collections_name, percentage, compute_similars, collection_out_iter_name)
+        for i in range(1, 11)
+    ])
+
+    compute_similars = "S"
+    percentage = 10
+    relatores = get_top_10_relatores()
+    for j, rel in enumerate(relatores):
+        new_query = query.copy()
+        new_query["relator"] = rel
+        collection_out_iter_name = "stf_kleinberg_acordaos_{}_rel_{}".format(percentage, (j + 1))
+        kleinberg_iters.extend([
+            (i, new_query, collections_name, percentage, compute_similars, collection_out_iter_name)
+            for i in range(1, 11)
+        ])
+
+    processes = 12
+    pool = Pool(processes=processes)
+    pool.map(run_hits_execution, kleinberg_iters)
+    pool.close()
+    pool.join()
+
+
+if __name__ == '__main__':
+    try:
+        tini = datetime.now()
+        run_acordaos_kleinberg_experiments()
+
+        os.system(
+            'echo "Kleinberg acabou com sucesso =)!" | mail -s "Kleinberg acabou!" -r "Jackson<jackson@ime.usp.br>" jackson@ime.usp.br'
+        )
+
+    except Exception as e:
+        os.system(
+            'echo %s | mail -s "Kleinberg falhou!" -r "Jackson<jackson@ime.usp.br>" jackson@ime.usp.br'
+            % e
+        )
+        with open("kleinberg_error.log", "a") as f:
+            f.write("%d: %s\n\n" % ((datetime.now() - tini).seconds, e))
